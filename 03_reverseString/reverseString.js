ac0c17dd35371b4fa2624debee1262149cd13cb0
Tyler Gregory <tylergreg95@gmail.com>
2024-05-03 17:39:22 -0400

    exercise 03 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let i = string.length; i >= 0; i--) {
+       reversedString += string.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
