2ba5f29ee2f177ec4b807c31e19e5f6940a01283
Tyler Gregory <tylergreg95@gmail.com>
2024-05-03 17:33:03 -0400

    exercise 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        let answer = "";
+        for(let i = 0; i < num; i++) {
+            answer += string;
+        }
+        return answer;
+    }
 
+    
 };
 
 // Do not edit below this line
