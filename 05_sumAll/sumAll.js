0c4d9426e921220f3f32ac2c6b177ececcde655e
Tyler Gregory <tylergreg95@gmail.com>
2024-05-03 23:09:59 -0400

    exercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let answer = 0;
+    
+    if (start < 0 || end < 0) {
+        return 'ERROR';
+    }
 
+    if (typeof start != "number" || typeof end != "number") {
+        return 'ERROR';
+    }
+
+    if (start > end) {
+        let tmp = start;
+        start = end;
+        end = tmp;
+    }
+
+    for (let i = start; i <= end; i++) {
+        answer += i;
+    }
+
+    return answer;
 };
 
 // Do not edit below this line
