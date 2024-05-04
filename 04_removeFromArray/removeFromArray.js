705b38f49db254c0f8da0f681a1270e060dcf6b0
Tyler Gregory <tylergreg95@gmail.com>
2024-05-03 22:59:11 -0400

    exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, num) {
+    
+    for (let j = 1; j < arguments.length; j++) {
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] === arguments[j]) {
+                arr.splice(i, 1);
+            }      
+        }
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
