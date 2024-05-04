4acbd4e9286b7f8baa4ad726be56ff0911c61f8b
Tyler Gregory <tylergreg95@gmail.com>
2024-05-04 09:01:24 -0400

    exercise 07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fDegree) {
+  result = fDegree - 32;
+  result *= (5/9);
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cDegree) {
+  result = cDegree * (9/5);
+  result += 32
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
