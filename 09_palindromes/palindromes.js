f9598c76db0039e1950a7392515788eeb694c64d
Tyler Gregory <tylergreg95@gmail.com>
2024-05-05 18:42:26 -0400

    Exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let strippedStringArr = string.match(/[abcdefghijklmnopqrstuvwxyz1234567890]/ig);
+    console.log(strippedStringArr);
+    let strippedString = "";
+    let reversedStrippedString = "";
+    for(let i = 0; i < strippedStringArr.length; i++) {
+        strippedString += strippedStringArr[i];
+        reversedStrippedString += strippedStringArr[strippedStringArr.length - (1 + i)];
+    }
+
+    console.log(strippedString);
+    console.log(reversedStrippedString);
+
+    if (strippedString.toLowerCase() == reversedStrippedString.toLowerCase()) {
+        return true;
+    }
+
+    return false;
 
 };
 
