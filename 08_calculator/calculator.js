6bdb0f947793284b5db62160e0e10622a3c31c44
Tyler Gregory <tylergreg95@gmail.com>
2024-05-05 14:10:12 -0400

    Exercise 08 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums.length === 0) { return 0; } 
+  return nums.reduce((total, num) => total + num);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => { return total * num}, 1);
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	answer = 1;
+  for(let i = 0; i < exp; i++) {
+    answer *= num;
+  }
+  return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) {
+    return 1;
+  } 
+  let answer = 1;
+  for (let i = 1; i <= a; i++) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
